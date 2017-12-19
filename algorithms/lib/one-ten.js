92a7519ca2974cc1b30cc235435fcfff24eef073
Alvin Kong <alvink92@gmail.com>
2017-12-18 20:22:24 -0800

    finshed twosum

--- algorithms/lib/one-ten.js
+++ algorithms/lib/one-ten.js
@@ -1,1 +1,39 @@
 // 1. Two Sum
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const cache = {};
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(typeof cache[target - nums[i]] !== 'undefined') {
+            return [cache[target - nums[i]], i];
+        }
+        if(!cache[nums[i]]){
+            cache[nums[i]] = i;
+        }
+    }
+};
+
+// console.log(twoSum([3,2,4], 6));
+// console.log(twoSum([3, 3], 6));
+
+
+// 2. Add Two Numbers
+
+//  Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+ 
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+};
