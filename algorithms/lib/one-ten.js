793c12143d817162f0741cc5dcadd9c13b06437d
Alvin Kong <alvink92@gmail.com>
2017-12-19 15:02:50 -0800

    finished addTwoNumbers

--- algorithms/lib/one-ten.js
+++ algorithms/lib/one-ten.js
@@ -34,6 +34,64 @@ function ListNode(val) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    
+
+let decNum = (listNode) => {
+    return listNode ? listNode.val : 0;
+}
+
+let nextNode = (listNode) => {
+    return listNode ? listNode.next : listNode;
+}
+
+var addTwoNumbers = function (l1, l2) {
+    let currDecSum = decNum(l1) + decNum(l2);
+    let onesPlace = new ListNode(currDecSum % 10);
+    let currCarryOver = parseInt(currDecSum / 10);
+    l1 = nextNode(l1);
+    l2 = nextNode(l2);
+
+    let prevDecPlace = onesPlace;
+
+    while (l1 || l2) {
+        currDecSum = currCarryOver + decNum(l1) + decNum(l2);
+        let nextDecPlace = new ListNode(currDecSum % 10);
+        prevDecPlace.next = nextDecPlace;
+        prevDecPlace = nextDecPlace;
+        currCarryOver = parseInt(currDecSum / 10);
+        l1 = nextNode(l1);
+        l2 = nextNode(l2);
+    }
+
+    prevDecPlace.val += currCarryOver;
+
+    return onesPlace;
 };
+
+// let decNum = function (l, decimalIndex) {
+//     return typeof l[decimalIndex] === 'undefined' ? 0 : l[decimalIndex];
+// }
+
+// var addTwoArrOfNumbers = function (l1, l2) {
+//     const sum = [];
+
+//     let currIdx = 0;
+//     let currDecSum;
+//     let prevCarryOver = 0;
+
+//     while (l1[currIdx] || l2[currIdx]) {
+//         currDecSum = prevCarryOver + decNum(l1, currIdx) + decNum(l2, currIdx);
+//         sum.push(currDecSum % 10);
+//         prevCarryOver = parseInt(currDecSum / 10);
+//         currIdx += 1;
+//     }
+
+//     if (prevCarryOver !== 0) {
+//         sum.push(prevCarryOver);
+//     }
+
+//     return sum;
+// };
+
+// console.log(addTwoArrOfNumbers([2, 4, 3], [5, 6, 4]));
+// console.log(addTwoArrOfNumbers([2, 4, 9, 3, 2], [5, 6, 4]));
+
