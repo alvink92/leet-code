af53597e4120598d347ed43e1ebb80c471ea969f
Alvin Kong <alvink92@gmail.com>
2017-12-20 15:01:42 -0800

    got non passing O(n^2) three sum

--- algorithms/eleven-twenty.js
+++ algorithms/eleven-twenty.js
@@ -27,3 +27,48 @@ var maxArea = function(heights) {
     
     return currMaxArea;
 };
+
+
+// 15. 3Sum
+
+// Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+// Note: The solution set must not contain duplicate triplets.
+
+// For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
+
+// possible solution: iterate first loop, then set target = 0 - nums[i], then the inner two nested loops solve like two sum with target instead of 0
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function(nums) {
+  let sums = [];
+  let exists = {};
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let target = 0 - nums[i];
+    // two sum here
+    let store = {};
+    for (let j = i + 1; j < nums.length; j++) {
+      let innerTarget = target - nums[j];
+      if (typeof store[innerTarget] !== "undefined") {
+        let existsString = [nums[i], innerTarget, nums[j]].sort().toString();
+        if (!exists[existsString]) {
+          sums.push([nums[i], innerTarget, nums[j]].sort());
+        }
+        exists[existsString] = true;
+      }
+      store[nums[j]] = true;
+    }
+  }
+  return sums;
+};
