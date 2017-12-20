53378a37d83f4626a6af29234db84ff8bc7b2124
Alvin Kong <alvink92@gmail.com>
2017-12-19 18:53:45 -0800

    finished longest substring in n^2

--- algorithms/lib/one-ten.js
+++ algorithms/lib/one-ten.js
@@ -69,6 +69,53 @@ var addTwoNumbers = function (l1, l2) {
     return onesPlace;
 };
 
+
+// 3. Longest Substring Without Repeating Characters
+
+// Given a string, find the length of the longest substring without repeating characters.
+
+// Examples:
+
+//     Given "abcabcbb", the answer is "abc", which the length is 3.
+
+// Given "bbbbb", the answer is "b", with the length of 1.
+
+// Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke"
+// is a subsequence and not a substring.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+let isUnique = (s) => {
+    let cache = {};
+    for (let i = 0; i < s.length; i++) {
+        if (cache[s[i]]) {
+            return false;
+        }
+        cache[s[i]] = true;
+    }
+    return true;
+}
+
+var lengthOfLongestSubstring = function (s) {
+    let longestSubStr = 0;
+    let leftPtr = 0;
+
+    for (let rightPtr = 0; rightPtr < s.length; rightPtr++) {
+        while (!isUnique(s.slice(leftPtr, rightPtr + 1))) {
+            leftPtr++;
+        }
+
+        if ((1 + rightPtr - leftPtr) > longestSubStr) {
+            longestSubStr = 1 + rightPtr - leftPtr;
+        }
+    }
+
+    return longestSubStr;
+};
+
+
 // 4. Median of Two Sorted Arrays
 
 // There are two sorted arrays nums1 and nums2 of size m and n respectively.
