a187db81e4c0986a20677fcdade9c3ae95a0db23
Alvin Kong <alvink92@gmail.com>
2017-12-20 19:07:20 -0800

    finshed removeDupes

--- algorithms/lib/21-30.js
+++ algorithms/lib/21-30.js
@@ -102,3 +102,38 @@ var swapPairs = function(head) {
   }
   return head;
 };
+
+
+// 26. Remove Duplicates from Sorted Array
+
+// Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+// It doesn't matter what you leave beyond the new length.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length <= 1) {
+        return nums.length;
+    }
+    
+    let barrierIdx = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[barrierIdx]) {
+            barrierIdx ++;
+            nums[barrierIdx] = nums[i];
+        }
+    }
+    
+    return nums.slice(0, barrierIdx + 1).length;
+};
