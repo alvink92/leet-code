df7d95d92ecc20aaf8bbd73b829681013ce70e4f
Alvin Kong <alvink92@gmail.com>
2017-12-20 19:20:33 -0800

    solved but not accepted

--- algorithms/lib/21-30.js
+++ algorithms/lib/21-30.js
@@ -137,3 +137,37 @@ var removeDuplicates = function(nums) {
     
     return nums.slice(0, barrierIdx + 1).length;
 };
+
+
+// 27. Remove Element
+
+// Given an array and a value, remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+// Example:
+
+// Given nums = [3,2,2,3], val = 3,
+
+// Your function should return length = 2, with the first two elements of nums being 2.
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let barrier = 0;
+    while(nums[++barrier] === val);
+    
+    for(let i = barrier + 1; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums[i] = nums[++barrier];
+        }
+    }
+    // console.log(nums.slice(barrier));
+    return nums.slice(barrier).length;
+};
