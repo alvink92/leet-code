6181f424364f3756e3b134e1cd8ed8dad3e775ed
Alvin Kong <alvink92@gmail.com>
2017-12-20 17:55:48 -0800

    finished merge two lists

--- algorithms/twentyone-thirty.js
+++ algorithms/twentyone-thirty.js
@@ -20,5 +20,47 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
+    let mergedHead;
+    let currMergedNode;
     
+    // edge case for null values
+    if (!l1 || !l2) {
+        let merged = l1 ? l1 : l2;
+        return merged;
+    }
+    
+    if (l1.val < l2.val) {
+        mergedHead = new ListNode(l1.val);
+        l1 = l1.next;
+    } else {
+        mergedHead = new ListNode(l2.val);
+        l2 = l2.next;
+    }
+    
+    currMergedNode = mergedHead;
+    
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            currMergedNode.next = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            currMergedNode.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        
+        currMergedNode = currMergedNode.next;
+    }
+    
+    currMergedNode.next = l1 ? l1 : l2;
+    
+    return mergedHead;
 };
+
+
+// 24. Swap Nodes in PairsGiven a linked list, swap every two adjacent nodes and return its head.
+
+// For example,
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+// Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
