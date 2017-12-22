990ebdf6102b319e95fbce29402c5f6889d7e668
Alvin Kong <alvink92@gmail.com>
2017-12-22 11:03:51 -0800

    finished last word

--- algorithms/lib/51-60.js
+++ algorithms/lib/51-60.js
@@ -29,3 +29,55 @@ var maxSubArray = function(nums) {
     
     return maxSum;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 58. Length of Last Word
+
+// Given a string s consists of upper / lower -
+//     case alphabets and empty space characters ' ',
+//     return the length of last word in the string.
+
+// If the last word does not exist,
+// return 0.
+
+// Note: A word is defined as a character sequence consists of non - space characters only.
+
+// Example:
+
+//     Input: "Hello World"
+// Output: 5
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    s = s.trim();
+    let length = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === " ") {
+            length = 0;
+        } else {
+            length++;
+        }
+    }
+    return length;
+};
