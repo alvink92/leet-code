3b55a6301ae76af7e70a7f20878ddd4d2bf3db55
Alvin Kong <alvink92@gmail.com>
2017-12-24 11:49:37 -0800

    init directions

--- algorithms/lib/51-60.js
+++ algorithms/lib/51-60.js
@@ -52,7 +52,8 @@ var maxSubArray = function(nums) {
  * @return {number[]}
  */
 var spiralOrder = function (matrix) {
-
+    const dir = {up: [-1, 0], down: [1, 0], left: [0, -1], right: [0, 1]};
+    
 };
 
 
