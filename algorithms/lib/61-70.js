91f749d0b4b62626ba2ec203ef3fac2cf66e7955
Alvin Kong <alvink92@gmail.com>
2017-12-31 14:26:06 -0800

    init 61-70.js file

---
+++ algorithms/lib/61-70.js
@@ -0,0 +1,26 @@
+// 61. Rotate List
+
+// Given a list, rotate the list to the right by k places, where k is non-negative.
+
+
+// Example:
+
+// Given 1->2->3->4->5->NULL and k = 2,
+
+// return 4->5->1->2->3->NULL.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    
+};
